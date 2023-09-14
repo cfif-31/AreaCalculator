77bb21af9d8ce4e3d93a804bb4adf104d23dfc85
cfif31 <i.cfif31@gmail.com>
2023-09-14 11:40:51 +0400

    add Circle

---
+++ SquareCalculator/Figures/Circle.cs
@@ -0,0 +1,14 @@
+﻿namespace SquareCalculator.Figures
+{
+    internal class Circle : Figure
+    {
+        private double Radius { get; }
+
+        public override double Square => 2 * Math.PI * Radius;
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+    }
+}
