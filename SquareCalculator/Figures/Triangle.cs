3bfe9c4fbc53fdf0a47db1a4cc8d6eb0eb4ccad0
cfif31 <i.cfif31@gmail.com>
2023-09-14 13:45:26 +0400

    add Triangle

---
+++ SquareCalculator/Figures/Triangle.cs
@@ -0,0 +1,27 @@
+﻿namespace SquareCalculator.Figures
+{
+    public class Triangle : Figure
+    {
+        public double SideA { get; }
+        public double SideB { get; }
+        public double SideC { get; }
+
+        public Triangle(double sideA, double sideB, double sideC)
+        {
+            SideA = sideA;
+            SideB = sideB;
+            SideC = sideC;
+        }
+
+        public override double Square
+        {
+            get
+            {
+                double halfPerimeter = (SideA + SideB + SideC) / 2;
+                double square = Math.Sqrt(halfPerimeter * (halfPerimeter - SideA) * (halfPerimeter - SideB) * (halfPerimeter - SideC));
+
+                return square;
+            }
+        }
+    }
+}
