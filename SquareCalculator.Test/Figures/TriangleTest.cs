05ef2a350ad0647581c0c0e8911df31553b7ff43
cfif31 <i.cfif31@gmail.com>
2023-09-16 01:32:42 +0400

    remove negative test

--- SquareCalculator.Test/Figures/TriangleTest.cs
+++ SquareCalculator.Test/Figures/TriangleTest.cs
@@ -16,36 +16,6 @@ namespace SquareCalculator.Test.Figures
         }
 
         [Test]
-        [TestCase(-1, 4, 2)]
-        [TestCase(-7, 13, 5)]
-        [TestCase(-12, 25, 10)]
-        public void NegativeTestA(double sideA, double sideB, double sideC)
-        {
-            var ex = Assert.Throws<ArgumentOutOfRangeException>(() => new Triangle(sideA, sideB, sideC));
-            Assert.That(ex.Message, Is.EqualTo($"Value ({sideA}) most be greather 0 (Parameter 'sideA')"));
-        }
-
-        [Test]
-        [TestCase(4, -1, 2)]
-        [TestCase(13, -7, 5)]
-        [TestCase(25, -12, 10)]
-        public void NegativeTestB(double sideA, double sideB, double sideC)
-        {
-            var ex = Assert.Throws<ArgumentOutOfRangeException>(() => new Triangle(sideA, sideB, sideC));
-            Assert.That(ex.Message, Is.EqualTo($"Value ({sideB}) most be greather 0 (Parameter 'sideB')"));
-        }
-
-        [Test]
-        [TestCase(4, 2, -1)]
-        [TestCase(13, 5, -7)]
-        [TestCase(25, 1, -12)]
-        public void NegativeTestC(double sideA, double sideB, double sideC)
-        {
-            var ex = Assert.Throws<ArgumentOutOfRangeException>(() => new Triangle(sideA, sideB, sideC));
-            Assert.That(ex.Message, Is.EqualTo($"Value ({sideC}) most be greather 0 (Parameter 'sideC')"));
-        }
-
-        [Test]
         [TestCase(3,5,8)]
         [TestCase(7,5,2)]
         [TestCase(4,9,5)]
