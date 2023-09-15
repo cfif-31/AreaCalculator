c380a97c9cd3566bf2dbfaa4ed2a3a4b7cd9bff0
cfif31 <i.cfif31@gmail.com>
2023-09-15 11:03:56 +0400

    Add valid Triangle check

--- SquareCalculator/Figures/Triangle.cs
+++ SquareCalculator/Figures/Triangle.cs
@@ -13,9 +13,12 @@
         /// <param name="sideA">Side A value</param>
         /// <param name="sideB">Side B value</param>
         /// <param name="sideC">Side C value</param>
-        /// <exception cref="ArgumentException">When one or more value less than or equal to 0</exception>
+        /// <exception cref="ArgumentException">When uncorrect triangle one or more value less than or equal to 0</exception>
         public Triangle(double sideA, double sideB, double sideC)
         {
+            if (sideA + sideB <= SideC)
+                throw new ArgumentException($"Triangle {SideA} {SideB} {SideC} does not exist!");
+
             SideA = GetCheckGreeterZeroValue(sideA, nameof(sideA));
             SideB = GetCheckGreeterZeroValue(sideB, nameof(sideB));
             SideC = GetCheckGreeterZeroValue(sideC, nameof(sideC));
